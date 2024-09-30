de4bacb808e4d7ea2a18402b2f78b7ac96dbd5ed
kingofgreatnezzz <kingofgreatnezzz@gmail.com>
2024-09-30 04:16:42 -0700

    new file

--- product_mgmt/settings.py
+++ product_mgmt/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'product',
-   'drf_yasg'
+   'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- product_mgmt/urls.py
+++ product_mgmt/urls.py
@@ -17,7 +17,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('admin-only/', admin.site.urls),
+    path('keys/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include('product.urls')),
 ]
